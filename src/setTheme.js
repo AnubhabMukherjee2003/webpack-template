b56a62326b9831ced28b30bbc907915a3fd49caa
AnubhabMukherjee2003 <antucruse621@gmail.com>
2025-01-16 10:35:32 +0530

    Deployment commit

--- src/setTheme.js
+++
@@ -1,8 +0,0 @@
-function setTheme(e) {
-    const root = document.documentElement;
-    const newTheme = root.className === 'dark' ? 'light' : 'dark';
-    root.className = newTheme;
-    localStorage.setItem('theme', newTheme);
-}
-
-export { setTheme };
